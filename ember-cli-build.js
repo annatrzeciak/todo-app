d60883662ebc5d3d978d2e604ae7de1a82f82297
Anna Trzeciak <trzeciakanna@hotmail.com>
2018-10-30 14:09:34 +0100

    create main  navbar

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -20,5 +20,11 @@ module.exports = function(defaults) {
   // please specify an object with the list of modules as keys
   // along with the exports of each module as its value.
 
+  app.import('bower_components/bootstrap/dist/js/bootstrap.min.js');
+  app.import('bower_components/bootstrap/dist/css/bootstrap.min.css');
+  app.import('bower_components/patternfly/dist/js/patternfly.min.js');
+  app.import('bower_components/patternfly/dist/css/patternfly.min.css');
+  app.import('bower_components/patternfly/dist/css/patternfly-additions.min.css');
+
   return app.toTree();
 };
